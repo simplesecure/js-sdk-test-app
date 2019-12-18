5126d34a376d4ecdaa9de02494fe4fc9852d3a1f
polluterofminds <justin@graphitedocs.com>
2019-12-17 21:48:21 -0500

    web3 work

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React from 'react';
 import './App.css';
 import SimpleID from 'simpleid-js-sdk';
 import { abi, bytecode } from './contractDeets';
+const Box = require('3box')
+const Web3 = require('web3');
 const simple = new SimpleID({
-  appOrigin: "https://app.graphitedocs.com",
-  scopes: ['store_write', 'publish_data'],
+  appOrigin: "http://localhost:3001",
+  appName: "Test App",
+  scopes: ['email'],
   apiKey: "123456",
   devId: "justin.email.email@email.com",
   development: true, 
-  network: 'layer2', 
+  network: 'ropsten', 
   localRPCServer: 'http://localhost:7545'
 });
-const address = "0xEC165904f967791C10de91CEeb57e65505063EBf";
+const web3 = new Web3(simple.getProvider());
+const address = "0xe9F3fe303dAe6223696cE3CE1573D7168100E8E8";
 const BLOCKSTACK_FILE_NAME = "SimpleID";
 let code;
 let email;
@@ -19,183 +23,291 @@ let valueToSet;
 let contract;
 let content;
 
-async function auth() {
-  const payload = { email };
-  const account = await simple.authenticate(payload);
-  console.log(account);
-}
-
-async function signIn() {
-  console.log(code);
-  const payload = { email, token: code};
-  const account = await simple.authenticate(payload);
-  console.log(account);
-}
-
-async function createContractTx() {
-  const data = simple.getUserData();
-  const account = data.wallet.ethAddr;
-  const payload = {
-    email: data.email, 
-    fromEmail: "hello@simpleid.xyz", 
-    bytecode, 
-    abi, 
-    account
+class App extends React.Component {
+  async componentDidMount() {
+    const accounts = await web3.eth.getAccounts();
+    console.log(accounts);
   }
-  const contract = await simple.createContract(payload);
-  console.log(contract);
-}
 
-async function fetchContract() {
-  contract = await simple.fetchContract(abi, address);
-  const taskCount = await contract.taskCount();
-  console.log(taskCount.toNumber());
-  let i;
-  let taskArr = [];
-  for (i = 1; i < taskCount.toNumber() + 1; i++) {
-    let task = await contract.tasks(i);
-    const taskObj = {
-      id: task[0],
-      content: task[1],
-      completed: task[2]
+  openBox = async () => {
+    const addr = simple.getUserData().wallet.ethAddr;
+    const box = await Box.openBox(addr, web3.currentProvider);
+    console.log(box);
+  }
+
+  auth = async () => {
+    const payload = { email };
+    const account = await simple.authenticate(payload);
+    console.log(account);
+  }
+  
+  signIn = async () => {
+    simple.signUserIn();
+    // console.log(code);
+    // const payload = { email, token: code};
+    // const account = await simple.authenticate(payload);
+    // console.log(account);
+  }
+  
+  createContractTx = async () => {
+    const data = simple.getUserData();
+    const account = data.wallet.ethAddr;
+    const payload = {
+      email: data.email, 
+      fromEmail: "hello@simpleid.xyz", 
+      bytecode, 
+      abi, 
+      account
     }
-    console.log(taskObj)
-    taskArr.push(taskObj);
+    const contract = await simple.createContract(payload);
+    console.log(contract);
   }
-}
-
-async function deployContract() {
-  const params = {
-    email: simple.getUserData().email, 
-    code
+  
+  fetchContract = async () => {
+    contract = new web3.eth.Contract(abi, address);
+    const taskCount = await contract.methods.userCount();
+    // contract = await simple.fetchContract(abi, address);
+    // const taskCount = await contract.taskCount();
+    // console.log(taskCount.toNumber());
+    // let i;
+    // let taskArr = [];
+    // for (i = 1; i < taskCount.toNumber() + 1; i++) {
+    //   let task = await contract.tasks(i);
+    //   const taskObj = {
+    //     id: task[0],
+    //     content: task[1],
+    //     completed: task[2]
+    //   }
+    //   console.log(taskObj)
+    //   taskArr.push(taskObj);
+    // }
   }
-  const contract = await simple.deployContract(params);
-  console.log(contract);
-  if(contract.address) {
-    pollForStatus(contract.deployTransaction.hash);
-  } else {
-    console.log("ERROR: ", contract);
+  
+  deployContract = async () => {
+    const contract = new web3.eth.Contract(abi, {
+      from: simple.getUserData().wallet.ethAddr, gas: 1000000, data: bytecode
+    });
+    //const newContract = await contract.new({from: simple.getUserData().wallet.ethAddr, gas: 1000000, data: bytecode})
+    contract.deploy({data: bytecode})
+    .send({
+      from: simple.getUserData().wallet.ethAddr,
+      gas: 1500000,
+      gasPrice: '20000000000'
+    })
+    .on('error', function(error){ 
+      console.log("ERROR: ", error)
+    })
+    .on('transactionHash', function(transactionHash){
+      console.log("HASH: ", transactionHash)
+    })
+    .on('receipt', function(receipt){
+      console.log("ADDRESS: ", receipt.contractAddress) // contains the new contract address
+    })
+    .on('confirmation', function(confirmationNumber, receipt){
+      console.log("CONFIRMATION NUMBER: ", confirmationNumber);
+    })
+    .then(function(newContractInstance){
+        console.log(newContractInstance.options.address) // instance with the new contract address
+    });
+    // const params = {
+    //   email: simple.getUserData().email, 
+    //   code
+    // }
+    // const contract = await simple.deployContract(params);
+    // console.log(contract);
+    // if(contract.address) {
+    //   this.pollForStatus(contract.deployTransaction.hash);
+    // } else {
+    //   console.log("ERROR: ", contract);
+    // }
   }
-}
-
-async function pollForStatus(tx) {
-  const status = await simple.pollForStatus(tx);
-  console.log(status);
-  if(status !== "Mined") {
-    pollForStatus(tx);
-  } else {
-    fetchContract();
-  }
-}
-
-// async function sendTokens() {
-//   //This will be a thing eventually
-// }
-
-async function setValue() {
-  const data = simple.getUserData();
-  const account = data.wallet.ethAddr;
-  await fetchContract();
-  const params = {
-    method: "createTask", 
-    value: valueToSet, 
-    abi, 
-    email: data.email, 
-    fromEmail: "hello@simpleid.xyz", 
-    account, 
-    address
-  }
-  const approval = await simple.createContractTransaction(params);
-  console.log(approval);
-}
-
-async function approveTransaction() {
-  const params = {
-    email: "justin.edward.hunter@gmail.com", 
-    contractTx: true, 
-    code
-  }
-  const transaction = await simple.broadcastTransaction(params);
-  console.log(transaction);
-  if(transaction.success) {
-    pollForStatus(transaction.body.hash);
-  } else {
-    console.log("ERROR: ", transaction);
-  }
-}
-
-async function postToBlockstack() {
-  const userSession = simple.getBlockstackSession();
-  const data = await userSession.putFile(BLOCKSTACK_FILE_NAME, JSON.stringify(content));
-  console.log(data);
-}
-
-async function fetchFromBlockstack() {
-  const userSession = simple.getBlockstackSession();
-  const data = await userSession.getFile(BLOCKSTACK_FILE_NAME);
-  console.log(data);
-}
-
-async function pinToIPFS() {
-  const params = {
-    email: simple.getUserData().email, 
-    id: "testing", 
-    content: {
-      title: "SimpleID Test", 
-      content
+  
+  pollForStatus = async (tx) => {
+    const status = await simple.pollForStatus(tx);
+    console.log(status);
+    if(status !== "Mined") {
+      this.pollForStatus(tx);
+    } else {
+      this.fetchContract();
     }
   }
-  const pinned = await simple.pinContent(params);
-  console.log(pinned);
-}
-
-async function fetchFromIPFS() {
-  const params = {
-    email: simple.getUserData().email, 
-    id: "testing"
+  
+  // async function sendTokens() {
+  //   //This will be a thing eventually
+  // }
+  
+  setValue = async () => {
+    const data = simple.getUserData();
+    const account = data.wallet.ethAddr;
+    await this.fetchContract();
+    const params = {
+      method: "createTask", 
+      value: valueToSet, 
+      abi, 
+      email: data.email, 
+      fromEmail: "hello@simpleid.xyz", 
+      account, 
+      address
+    }
+    const approval = await simple.createContractTransaction(params);
+    console.log(approval);
   }
-  const fetchedContent = await simple.fetchPinnedContent(params);
-  console.log(fetchedContent);
-}
+  
+  approveTransaction = async () => {
+    const approval = await web3.eth.approveTransaction({
+      from: "0x709eDA1E2cc771C5D17a86422C121785eD7c2Cc0",
+      gasPrice: "20000000000",
+      gas: "21000",
+      to: '0x3535353535353535353535353535353535353535',
+      value: "1000000000000000000",
+      data: "0x"
+    });
+    console.log(approval);
+    // const params = {
+    //   email: simple.getUserData().email, 
+    //   contractTx: true, 
+    //   code
+    // }
+    // const transaction = await simple.broadcastTransaction(params);
+    // console.log(transaction);
+    // if(transaction.success) {
+    //   this.pollForStatus(transaction.body.hash);
+    // } else {
+    //   console.log("ERROR: ", transaction);
+    // }
+  }
+  
+  postToBlockstack = async () => {
+    const userSession = simple.getBlockstackSession();
+    const data = await userSession.putFile(BLOCKSTACK_FILE_NAME, JSON.stringify(content));
+    console.log(data);
+  }
+  
+ fetchFromBlockstack = async () => {
+    const userSession = simple.getBlockstackSession();
+    const data = await userSession.getFile(BLOCKSTACK_FILE_NAME);
+    console.log(data);
+  }
+  
+  pinToIPFS = async () => {
+    const params = {
+      email: simple.getUserData().email, 
+      id: "testing", 
+      content: {
+        title: "SimpleID Test", 
+        content
+      }
+    }
+    const pinned = await simple.pinContent(params);
+    console.log(pinned);
+  }
+  
+  fetchFromIPFS = async () => {
+    const params = {
+      email: simple.getUserData().email, 
+      id: "testing"
+    }
+    const fetchedContent = await simple.fetchPinnedContent(params);
+    console.log(fetchedContent);
+  }
+  
+  handleCode = (e) => {
+    code = e.target.value;
+  }
+  
+  handleValue = (e) => {
+    valueToSet = e.target.value;
+  }
+  
+  handleEmail = (e) => {
+    email = e.target.value;
+  }
+  
+  handleContent = (e) => {
+    content = e.target.value;
+  }
 
-function handleCode(e) {
-  code = e.target.value;
-}
+  signTx = async () => {
+    try {
+      web3.eth.sendTransaction({
+        from: simple.getUserData().wallet.ethAddr,
+        gasPrice: "20000000000",
+        gas: "21000",
+        to: '0x3ba190E767c1C1bfa7b0e3181829bBCBe82cfcD7',
+        value: "0000005",
+        data: "0x"
+      })
+      .on('transactionHash', (hash) => {
+        console.log(hash)
+      })
+    } catch(e) {
+      console.log("IN APP ERROR: ", e);
+    }
+    // console.log("SIGNED IN APP ", signed);
+  }
 
-function handleValue(e) {
-  valueToSet = e.target.value;
-}
+  estGas = async () => {
+    const estimate = await web3.eth.estimateGas({
+      to: "0x11f4d0A3c12e86B4b5F39B213F7E19D048276DAe",
+      data: "0xc6888fa10000000000000000000000000000000000000000000000000000000000000003"
+    });
+    console.log(estimate);
+  }
 
-function handleEmail(e) {
-  email = e.target.value;
-}
+  signMessage = async () => {
+    const signedMsg = await web3.eth.sign("Hello world", simple.getUserData().wallet.ethAddr);
+    console.log("SIGNED", signedMsg);
+  }
 
-function handleContent(e) {
-  content = e.target.value;
-}
+  signOut = () => {
+    simple.signOut();
+  }
 
-function App() {
-  console.log(simple.getProvider());
-  console.log(simple.getUserData());
-  return (
-    <div className="App">
-      <button onClick={auth}>Auth Token</button>
-      <button onClick={signIn}>Login With Token</button>
-      <button onClick={pinToIPFS}>Store IPFS</button>
-      <button onClick={fetchFromIPFS}>Fetch IPFS</button>
-      <button onClick={postToBlockstack}>Store Blockstack</button>
-      <button onClick={fetchFromBlockstack}>Fetch Blockstack</button>
-      <button onClick={fetchContract}>Fetch Eth Contract</button>
-      <button onClick={createContractTx}>Create Contract</button>
-      <button onClick={deployContract}>Deploy Contract</button>
-      <button onClick={setValue}>Update Contract</button>
-      <button onClick={approveTransaction}>Approve Transaction</button> <br/>
-      <input type="email" placeholder="email" onChange={handleEmail} /> <br/>
-      <input type="text" placeholder="token" onChange={handleCode} /> <br/>
-      <input type="text" placeholder="Set contract value" onChange={handleValue} /><br/>
-      <input type="text" placeholder="Type some content" onChange={handleContent} />
-    </div>
-  );
+  updateState = () => {
+    this.setState({ updated: true });
+  }
+
+  render() {
+    console.log(simple.getProvider());
+    console.log(simple.getUserData());
+    let loggedIn = false;
+    if(simple.getUserData()) {
+      loggedIn = true;
+    }
+    return (
+      <div className="App">
+        {
+          loggedIn ?
+          <div>
+            <h1>Welcome back!</h1>
+            <p>Here's your wallet address: {simple.getUserData().wallet.ethAddr}</p>
+          </div> : 
+          <h1>Log in to continue</h1>
+        }
+        <button onClick={this.auth}>Auth Token</button>
+        <button onClick={this.signIn}>Login With Token</button>
+        <button onClick={this.pinToIPFS}>Store IPFS</button>
+        <button onClick={this.fetchFromIPFS}>Fetch IPFS</button>
+        <button onClick={this.postToBlockstack}>Store Blockstack</button>
+        <button onClick={this.fetchFromBlockstack}>Fetch Blockstack</button>
+        <button onClick={this.fetchContract}>Fetch Eth Contract</button>
+        <button onClick={this.createContractTx}>Create Contract</button>
+        <button onClick={this.deployContract}>Deploy Contract</button>
+        <button onClick={this.setValue}>Update Contract</button>
+        <button onClick={this.signTx}>Sign Transaction</button> <br/>
+        <button onClick={this.signMessage}>Sign Message</button> <br/>
+        <button onClick={this.estGas}>Estimate Gas</button> <br/>
+        <button onClick={this.approveTransaction}>Approve Transaction</button> <br/>
+        <button onClick={this.openBox}>3Box</button> <br/>
+        <button onClick={this.signOut}>Sign Out</button> <br/>
+        <input type="email" placeholder="email" onChange={this.handleEmail} /> <br/>
+        <input type="text" id="token" placeholder="token" onChange={this.handleCode} /> <br/>
+        <input type="text" placeholder="Set contract value" onChange={this.handleValue} /><br/>
+        <input type="text" placeholder="Type some content" onChange={this.handleContent} />
+      </div>
+    );
+  }
+  
 }
 
 export default App;
